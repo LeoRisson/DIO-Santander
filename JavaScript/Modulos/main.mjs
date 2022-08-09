77695d4c78d8d63afe9714253484a5572cbb0940
LeoRisson <leonardo_risson@hotmail.com>
2022-08-09 10:32:23 -0300

    Curso JS, Modulos.

---
+++ JavaScript/Modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log (mostraIdade('Leo', 43));
+console.log (mostraCidade('Leo', 'Muriae-MG'));
+console.log (mostraHobby('Leo', 'Video Games'));
+
